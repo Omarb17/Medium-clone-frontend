ef5bb39fd8b11ccfd98af30d132941f735beb2f2
Omar Bouslime <bouslimeomar@gmail.com>
2026-08-14 22:34:08 +0100

    feat-add-sidebar-widget

--- src/_pages/Home/ui/HomePage.tsx
+++ src/_pages/Home/ui/HomePage.tsx
@@ -1,11 +1,24 @@
+"use client";
+import Sidebar from "@/src/widgets/Sidebar";
 import Navbar from "../../../widgets/Navbar";
+import { useState } from "react";
 
-export default function HomePage()  {
+export default function HomePage() {
+  const [open, setOpen] = useState(true);
+
+  const handleSidebarOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <Navbar />
-    <h1>Home</h1>
+      <Navbar handleSidebarOpen={handleSidebarOpen} />
+      <Sidebar />
+      <h1>Home</h1>
     </>
-  )
+  );
 }
-
